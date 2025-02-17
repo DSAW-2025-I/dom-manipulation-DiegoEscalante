2c80ca4232e6dc2c6608c70ddb0e9c0b3af30d78
unknown <descalanteg05@gmail.com>
2025-02-17 17:39:55 -0500

    Finished the Javascript logic for the carousel

--- script.js
+++ script.js
@@ -5,42 +5,53 @@ const leftButton = document.querySelector(".left-button");
 const rightButton = document.querySelector(".right-button");
 const imageCarousel = document.querySelector(".image-carousel")
 
+if(currentIndex === 0){
+    leftButton.style.cursor = "not-allowed";
+    leftButton.style.opacity = "0.5";
+}
+
 leftButton.addEventListener("click", () => {
     if (currentIndex > 0) {
-        imageCarousel.style.transition = "opacity 0.6s ease";
+        imageCarousel.style.transition = "opacity 0.4s ease";
         imageCarousel.style.opacity = 0;
+        leftButton.style.cursor = "pointer";
+        leftButton.style.opacity = "1";
         currentIndex--;
         setTimeout(()=>{
             imageCarousel.src = images[currentIndex];
             imageCarousel.style.opacity = 1;
-        }, 300);
-    }
+        }, 400);
+        rightButton.style.cursor = "pointer";
+        rightButton.style.opacity = "1";
+        if(currentIndex === 0){
+            leftButton.style.cursor = "not-allowed";
+            leftButton.style.opacity = "0.5";
+        }
+    } 
 });
 
 rightButton.addEventListener("click", () => {
     if (currentIndex < images.length - 1) {
-        imageCarousel.style.transition = "opacity 0.6s ease";
+        imageCarousel.style.transition = "opacity 0.4s ease";
         imageCarousel.style.opacity = 0;
+        rightButton.style.cursor = "pointer";
+        rightButton.style.opacity = "1";
         currentIndex++; 
         setTimeout(()=>{
-        imageCarousel.src = images[currentIndex];
-        imageCarousel.style.opacity = 1;
-    }, 300);
-}});
-
-leftButton.addEventListener("mouseenter",()=>{
-    if (currentIndex>0){
+            imageCarousel.src = images[currentIndex];
+            imageCarousel.style.opacity = 1;
+        }, 400);
         leftButton.style.cursor = "pointer";
         leftButton.style.opacity = "1";
-    } else{
-        leftButton.style.cursor = "not-allowed";
-        leftButton.style.opacity = "0.5";
+        if(currentIndex === images.length - 1){
+            rightButton.style.cursor = "not-allowed";
+            rightButton.style.opacity = "0.5";
+        }
+}});
 
 
-    }
-})
 
-rightButton.addEventListener("mouseenter",()=>{
+rightButton.addEventListener("mouseover",()=>{
     if (currentIndex< images.length - 1){
         rightButton.style.cursor = "pointer";
         rightButton.style.opacity = "1";
